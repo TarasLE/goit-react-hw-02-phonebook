ece67c93ddaed4614fbcf207d6a3acfbcd27714a
Taras Liuzniak <64332868+TarasLE@users.noreply.github.com>
2021-01-19 16:21:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,4 @@ import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(<App />, document.querySelector('#root'))
 
-const arr = [0, 6, 9, 4, 7]
-const arr2 = [{ 0: 1 }, { 6: 1 }, { 9: 5 }, { 4: 7 }, { 7: 8 }]
-// arr2.delete(2)
-console.log(arr2)
+
